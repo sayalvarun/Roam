855e3c5d60c97b46ad68b366f558e918e642517d
Varun <sayalvarun@gmail.com>
2015-09-26 18:30:25 -0400

    added ability to listen and send messages

--- MyApplication/app/src/main/java/com/example/varun/roam/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/varun/roam/MainActivity.java
@@ -1,21 +1,51 @@
 package com.example.varun.roam;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    EditText source = null;
+    EditText dest = null;
+    Button button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("16318962070", null, "Varun is awesome", null, null);
+        IntentFilter filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        registerReceiver(new SmsListener(),filter);
+        source = (EditText) findViewById(R.id.sourceTextField);
+        dest = (EditText) findViewById(R.id.destinationTextField);
+        button = (Button) findViewById(R.id.getDirectionsButton);
+        final SmsManager smsManager = SmsManager.getDefault();
+
+        button.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String content = "";
+                    content+= "from: "+source.getText().toString()+"\n";
+                    content+= "to: "+dest.getText().toString();
+                    smsManager.sendTextMessage("17328100017", null, content, null, null);
+                }
+            }
+        );
     }
+
+    //SmsManager smsManager = SmsManager.getDefault();
+    //smsManager.sendTextMessage("16318962070", null, "Varun is awesome", null, null);
+
 
 
     @Override
--- MyApplication/app/src/main/java/com/example/varun/roam/SmsListener.java
+++ MyApplication/app/src/main/java/com/example/varun/roam/SmsListener.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
@@ -16,26 +17,10 @@ public class SmsListener extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
-
-        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
-            SmsMessage[] msgs = null;
-            String msg_from;
-            if (bundle != null){
-                //---retrieve the SMS message received---
-                try{
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    msgs = new SmsMessage[pdus.length];
-                    for(int i=0; i<msgs.length; i++){
-                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-                        msg_from = msgs[i].getOriginatingAddress();
-                        String msgBody = msgs[i].getMessageBody();
-                        Log.d("Message was:",msgBody);
-                    }
-                }catch(Exception e){
-//                            Log.d("Exception caught",e.getMessage());
-                }
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                String messageBody = smsMessage.getMessageBody();
+                Log.d("MESSAGE: ",messageBody);
             }
         }
     }
