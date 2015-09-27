0aaa7fc05b4b6366f227c1304a9a4d2b71db317f
Varun <sayalvarun@gmail.com>
2015-09-27 05:30:16 -0400

    added so there is a new view with all the info

--- MyApplication/app/src/main/java/com/example/varun/roam/ListDirections.java
+++ MyApplication/app/src/main/java/com/example/varun/roam/ListDirections.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -19,12 +21,20 @@ public class ListDirections extends AppCompatActivity {
         String totalDuration = getIntent().getExtras().getString("totalDuration");
         String totalDistance = getIntent().getExtras().getString("totalDistance");
         ArrayList<String> elems = getIntent().getExtras().getStringArrayList("directions");
+        ArrayList<String> toStrings = new ArrayList<String>();
         Direction[] directions = new Direction[elems.size()-1];
         for(int i=0; i<directions.length; i++){
             String[] subElems = elems.get(i).split(";");
             directions[i] = new Direction(subElems[0],subElems[1],subElems[2]);
+            toStrings.add(directions[i].toString());
             Log.d("ELEM:",directions[i].toString());
         }
+
+        ArrayAdapter<String> itemsAdapter;
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toStrings);
+
+        ListView listView = (ListView) findViewById(R.id.directions_view);
+        listView.setAdapter(itemsAdapter);
 
         // /Log.d("Dur: ", totalDuration);
     }
