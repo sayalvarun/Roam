eb35099e13b9111740f950ab8ccb528db9990ef3
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-09-27 11:13:20 -0400

    weather icons

--- MyApplication/app/src/main/java/com/example/varun/roam/DisplayWeather.java
+++ MyApplication/app/src/main/java/com/example/varun/roam/DisplayWeather.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.github.pwittchen.weathericonview.WeatherIconView;
+
 
 public class DisplayWeather extends ActionBarActivity {
 
@@ -16,12 +18,39 @@ public class DisplayWeather extends ActionBarActivity {
         setContentView(R.layout.activity_display_weather);
         String today = getIntent().getExtras().getString("todaysWeather");
         String tomorrow = getIntent().getExtras().getString("tomorrowsWeather");
+        String todayIcon = getIntent().getExtras().getString("todaysWeatherIcon");
+        String tomorrowIcon = getIntent().getExtras().getString("tomorrowsWeatherIcon");
+
+        WeatherIconView todayIconView = (WeatherIconView) findViewById(R.id.today_weather_icon);
+        WeatherIconView tomorrowIconView = (WeatherIconView) findViewById(R.id.today_weather_icon);
+
+        setIcon(todayIconView, todayIcon);
+        setIcon(tomorrowIconView, tomorrowIcon);
+
         TextView todayview = (TextView) findViewById(R.id.todays_weather);
         TextView tomorrowview = (TextView) findViewById(R.id.tomorrows_weather);
         todayview.setText(today);
         tomorrowview.setText(tomorrow);
     }
 
+
+    private void setIcon(WeatherIconView iconView, String iconAPI){
+
+        iconView.setIconResource(getString(R.string.wi_alien));
+
+        if(iconAPI.contains("clear-day")){
+            iconView.setIconResource(getString(R.string.wi_day_sunny));
+        }
+        if(iconAPI.contains("partly-cloudy-day")) {
+            iconView.setIconResource(getString(R.string.wi_day_cloudy));
+        }
+        if(iconAPI.contains("cloudy")) {
+            iconView.setIconResource(getString(R.string.wi_cloud));
+        }
+        if(iconAPI.contains("wind")) {
+            iconView.setIconResource(getString(R.string.wi_windy));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
