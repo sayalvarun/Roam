f17b65225be6bc5ef821e532d715c831f4d21f02
Amit Bapat <amitbap@gmail.com>
2015-09-26 21:39:25 -0400

    server script

--- serverside/parseMessages.py
+++ serverside/parseMessages.py
@@ -1,20 +1,72 @@
+import requests
+import pprint
+import string
+import re
+import os
+import json
+
 from flask import Flask, request, redirect
 import twilio.twiml
+
+def sendText(phone, message):
+    cmd = 'curl -X POST http://textbelt.com/text -d number='
+    cmd += phone
+    cmd += ' -d "message='
+    cmd += message
+    cmd += '"'
+    os.system(cmd)
  
+def getKey():
+    f = open(key.txt)
+    key = f.read()
+    return key
+
+def getURL(source, destination):
+    url = 'https://maps.googleapis.com/maps/api/directions/json?origin='
+    url += source
+    url += "&destination="
+    url += "&key="
+    url += getKey()
+
+def getDirections(source, destination):
+    url = getURL(source, destination)
+    r = requests.get(url)   
+    
+    obj = r.json()
+    distance = obj["routes"][0]["legs"][0]["distance"]["text"]
+    duration = obj["routes"][0]["legs"][0]["duration"]["text"]
+    out = str(distance) + ";" + str(duration)
+    steps = obj["routes"][0]["legs"][0]["steps"]
+    i = 1
+    for step in steps:
+        dist = step["distance"]["text"]
+        dur = step["duration"]["text"]
+        directions = step["html_instructions"]
+        directions = re.sub("<[^>]*>", '', directions)
+        out += ("~" + str(dist) + ";" + str(dur) + ";" + str(directions))
+        i = i + 1
+
+    return out
+
+###################################################################################################################################
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def recieveMessage():
     """Respond to incoming calls with a simple text message."""
     msg = request.values.get('Body', None)
+    recvNumber = reques.values.get('From', None)
     if msg is not None:
-    	print(msg)
+    	params = msg.split(";")
+        output = getDirections(params[0], params[1])
+        sendText(recvNumber, output)
+        print(output)
     else:
-   	msg = "Invalid" 
+   	    msg = "Invalid" 
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+    #resp = twilio.twiml.Response()
+    #resp.message("Hello, Mobile Monkey")
+    #return str(resp)
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
